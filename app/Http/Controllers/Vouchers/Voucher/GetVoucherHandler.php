02064829cc4a3482e52d3217638a9bf0adeee018
soinekro <echumaceroj@ucvvirtual.edu.pe>
2024-09-11 07:11:02 -0500

    endpoint api/v1/vouchers/{voucher}=>app/Http/Controllers/Vouchers/Voucher/GetVoucherHandler

--- app/Http/Controllers/Vouchers/Voucher/GetVoucherHandler.php
+++ app/Http/Controllers/Vouchers/Voucher/GetVoucherHandler.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers\Vouchers\Voucher;
 
+use App\Http\Resources\Vouchers\VoucherResource;
+use App\Models\Voucher;
+use App\Services\VoucherService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
 class GetVoucherHandler
 {
+    public function __construct(private readonly VoucherService $voucherService)
+    {
+    }
 
+    public function __invoke(Request $request, Voucher $voucher): Response
+    {
+        return response([
+            'data' => VoucherResource::make($voucher),
+        ], 200);
+    }
 }
--- routes/v1/vouchers/auth.php
+++ routes/v1/vouchers/auth.php
@@ -4,11 +4,15 @@ use App\Http\Controllers\Vouchers\GetVouchersHandler;
 use App\Http\Controllers\Vouchers\StoreVouchersHandler;
 use App\Http\Controllers\Vouchers\Voucher\DeleteVoucherHandler;
 use App\Http\Controllers\Vouchers\Voucher\GetVoucherHandler;
+use App\Http\Controllers\Vouchers\Voucher\StoreVoucherHandler;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('vouchers')->group(
     function () {
         Route::get('/', GetVouchersHandler::class);
         Route::post('/', StoreVouchersHandler::class);
+        Route::post('/store', StoreVoucherHandler::class);
+        Route::get('/{voucher}', GetVoucherHandler::class);
+        // Route::delete('/{voucher}', DeleteVoucherHandler::class);
     }
 );
